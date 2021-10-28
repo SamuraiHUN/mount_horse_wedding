b62e662f979395b33d28b7eb7a08cf3758a9b0a9
SamuraiHUN <89939630+SamuraiHUN@users.noreply.github.com>
2021-10-28 17:13:01 +0200

    Add files via upload

---
+++ block_wedding_horse_mount/01.src/game/cmd_general.cpp
@@ -0,0 +1,38 @@
+search this (in ACMD(do_user_horse_ride))
+
+	if (ch->IsHorseRiding() == false)
+
+after add this:
+
+	LPITEM armor = ch->GetWear(WEAR_BODY);
+	if (armor)
+	{
+		DWORD blockWeeding[4] = {11901, 11902, 11903, 11904};
+		for (auto armorVnum : blockWeeding) {
+			if (armor->GetVnum() == armorVnum)
+			{
+				ch->ChatPacket(CHAT_TYPE_INFO, LC_TEXT("you can't ride if you are in a fitted wedding suit/dress."));
+				return;
+			}
+		}
+	}
+
+search this (in ACMD(do_ride) )
+
+	if (ch->GetMapIndex() == 113)
+		return;
+
+before add this:
+
+	LPITEM armor = ch->GetWear(WEAR_BODY);
+	if (armor)
+	{
+		DWORD blockWeeding[4] = {11901, 11902, 11903, 11904};
+		for (auto armorVnum : blockWeeding) {
+			if (armor->GetVnum() == armorVnum)
+			{
+				ch->ChatPacket(CHAT_TYPE_INFO, LC_TEXT("you can't ride if you are in a fitted wedding suit/dress."));
+				return;
+			}
+		}
+	}
